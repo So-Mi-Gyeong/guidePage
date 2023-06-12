5b5213f4ced21efa8b96feb1862ac696a94654c8
SSSSOMI <somkung28@naver.com>
2023-06-12 10:49:54 +0900

    .

--- resource/js/guide-ui-script.js
+++ resource/js/guide-ui-script.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
   const myTab = () => {
     const tabList = document.querySelectorAll('.defualt_tab_list');  //ul 리스트
     for(const tabArea of tabList){   //tabArea  --> ul 
+      
       tabArea.addEventListener('click', function(e){  //ul에 클릭 이벤트
         const tabChild = this.children; // li 리스트
         
-        console.log(tabChild)
         //li를 모두 찾아서 class 값이 on인 거를 모두 제거
         for (const child of tabChild) { 
           if(child.classList.contains('on')){ 
